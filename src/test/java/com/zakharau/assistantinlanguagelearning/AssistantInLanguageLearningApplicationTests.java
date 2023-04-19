122b9d545c531cc44386efe2cba5828383148329
AndreyZ <a.zahar3111981@gmail.com>
2023-04-20 01:02:36 +0300

    start project, configure project

---
+++ src/test/java/com/zakharau/assistantinlanguagelearning/AssistantInLanguageLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakharau.assistantinlanguagelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantInLanguageLearningApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
